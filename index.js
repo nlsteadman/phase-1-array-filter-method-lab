fa66976c5239ddff2df60f42ed02e64c59d174fc
Nicole Steadman <nsteadman@gmail.com>
2021-10-18 15:14:45 -0700

    commit tree 76ca774ff68e3a27ad48aaffac202684e1a635fa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching (drivers, driver){
+    return drivers.filter(function(el){
+        return el.toLowerCase() === driver.toLowerCase();
+    })
+}
+
+function fuzzyMatch (drivers, driver){
+    return drivers.filter(function(letter){
+        return letter.toLowerCase().startsWith(driver.toLowerCase())
+    })
+}
+
+function matchName (drivers, driver){
+    return drivers.filter(function(hometown){
+        return hometown.name === driver
+        })
+}
